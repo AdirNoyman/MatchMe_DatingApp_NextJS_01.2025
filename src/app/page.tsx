499690df080872e81c8796c01ca77bc63afaf631
Adir Noyman <ofahol2007@hotmail.com>
2025-01-28 22:35:09 +0200

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Button } from '@heroui/react';
+import Link from 'next/link';
 import { FaRegSmile } from 'react-icons/fa';
 
 export default function Home() {
@@ -8,7 +9,7 @@ export default function Home() {
       <h1 className="text-3xl font-bold text-center text-purple-600">
         Hello Adiros 🤓
       </h1>
-      <Button color='primary' variant='bordered' startContent={<FaRegSmile size={20}/>}>Click me</Button>
+      <Button as={Link} href='/members' color='primary' variant='bordered' startContent={<FaRegSmile size={20}/>}>Members</Button>
     </div>
   );
 }
